bfaa8aec62cff0dd7a8f14618d0113c94c93b896
teejay80s <works.harbidoye@gmail.com>
2022-02-03 18:02:55 +0100

    header

--- src/component/header_/index.jsx
+++ src/component/header_/index.jsx
@@ -3,13 +3,16 @@ import styles from "./styles.module.css"
 function Header() {
   return( 
         <div className={styles.header}>
-          <div className={styles.leftHeader}>Edit Profile</div>
+          <div className={styles.leftHeader}>
+           <div>Edit Profile</div>
+            
+            </div>
           <div className={styles.rightHeader}>
               <ul>
-                <li className={styles.imgContainer}> yes </li>
-                <li>user name</li>
-                <li><button>Do you have any suggestion</button></li>
-                <li><button>Do you have a complain</button></li>
+                {/* <li className={styles.imgContainer}> yes </li>
+                <li>user name</li> */}
+                <li><button className={styles.orange}>Do you have any suggestion?</button></li>
+                <li><button className={styles.purple}>Do you have a complain?</button></li>
               </ul>
           </div> 
         </div>
